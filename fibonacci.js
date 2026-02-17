4367971edf27c73fc56981cd5dfe93de8c66b522
Aero Flamiano <aflamiano.career@gmail.com>
2026-02-17 23:28:57 +0800

    Fixed fibonacci recursive function

--- fibonacci.js
+++ fibonacci.js
@@ -10,11 +10,26 @@ function fibs(n) {
 
   return fib;
 }
-
-console.log(fibs(8));
+console.log(fibs(5));
 
 function fibsRec(n) {
-  if (n < 2) return n;
-  return fibsRec(n - 1) + fibsRec(n - 2);
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+
+  if (n === 2) {
+    return [0, 1];
+  } else {
+    let fib = fibsRec(n - 1);
+    fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+    return fib;
+  }
 }
-console.log(fibsRec(8));
+console.log(fibsRec(0));
+console.log(fibsRec(1));
+console.log(fibsRec(5));
+
+function fibsValue(n) {
+  if (n < 2) return n;
+  return fibsValue(n - 1) + fibsValue(n - 2);
+}
+console.log(fibsValue(3));
