62c0bf0791d9fdd0899f13541518d65854fa5ef3
Aero Flamiano <aflamiano.career@gmail.com>
2026-02-17 16:43:31 +0800

    Fixed collatz function

--- collatz.js
+++ collatz.js
@@ -1,27 +1,23 @@
 function collatz(n) {
-  let steps = 0;
-  steps++;
   if (n === 1) {
-    return 1;
-  }
-
-  if (n % 2 === 0) {
+    return 0;
+  } else if (n % 2 === 0) {
     console.log("even fired");
-    return steps + collatz(n / 2);
+    return 1 + collatz(n / 2);
   } else {
     console.log("odd fired");
-    return steps + collatz(3 * n + 1);
+    return 1 + collatz(3 * n + 1);
   }
 }
 
-console.log(collatz(3));
+console.log(collatz(6));
 
-// step(6 / 2);
-// step(3 * 3 + 1);
-// step(10 / 2);
-// step(3 * 5 + 1);
-// step(16 / 2);
-// step(8 / 2);
-// step(4 / 2);
-// step(2 / 2);
-// step(1);
+// step(6 / 2); 1 +
+// step(3 * 3 + 1); 1 +
+// step(10 / 2); 1 +
+// step(3 * 5 + 1); 1 +
+// step(16 / 2); 1 +
+// step(8 / 2); 1 +
+// step(4 / 2); 1 +
+// step(2 / 2); 1 +
+// step(1); 0
